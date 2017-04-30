9a2067b181a24cbda6c3305048a73c48fb374842
Kim Toy (Personal) <tokutei12@gmail.com>
2017-04-29 17:20:01 -0700

    Layout storyboard for journal, entry, and search views. No autolayout

---
+++ imagejourney/JournalEntryCell.swift
@@ -0,0 +1,24 @@
+//
+//  JournalEntryCell.swift
+//  imagejourney
+//
+//  Created by Kim Toy (Personal) on 4/29/17.
+//  Copyright © 2017 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class JournalEntryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
