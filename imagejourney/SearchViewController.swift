fe0e8ee02d0a649acbb8a3327cf9b9a2b981eb34
Kim Toy (Personal) <tokutei12@gmail.com>
2017-04-29 17:20:01 -0700

    Layout storyboard for journal, entry, and search views. No autolayout

---
+++ imagejourney/SearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewController.swift
+//  imagejourney
+//
+//  Created by Kim Toy (Personal) on 4/29/17.
+//  Copyright © 2017 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
