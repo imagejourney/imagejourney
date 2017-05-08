38e530ce932fcc12420d6eb3b2c2bcb75b2f7522
sophia <feiyang.f@gmail.com>
2017-05-07 22:57:35 -0700

    Add date picker. Rename compose entry again, trying to fit all of compose entry in one view

--- imagejourney/ComposeEntryLocationPickerViewController.swift
+++ imagejourney/ComposeEntryLocationPickerViewController.swift
@@ -11,12 +11,53 @@ import UIKit
 
 class ComposeEntryLocationPickerViewController: UIViewController, UISearchBarDelegate {
 
+    var datePicker: UIDatePicker!
+    var datePickerToolBar: UIToolbar!
+    var showingDatePicker: Bool! = false
+    
     @IBAction func onCancel(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
   
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBAction func onTapDateLabel(_ sender: UITapGestureRecognizer) {
+        if showingDatePicker {
+            return
+        }
+        
+        showingDatePicker = true
+        datePicker = UIDatePicker.init(frame: CGRect(x: 0, y: self.view.height - 200, width: self.view.width, height: 200))
+        datePicker.backgroundColor = UIColor.white
+        datePicker.datePickerMode = UIDatePickerMode.date;
+        datePicker.isHidden = false;
+        datePicker.date = Date()
+        datePicker.addTarget(self, action: #selector(updateDateLabelTitle), for: UIControlEvents.valueChanged)
+        self.view.addSubview(datePicker)
+        
+        datePickerToolBar = UIToolbar.init(frame: CGRect(x: 0, y: self.view.height - 200 - 40, width: self.view.width, height: 40))
+        datePickerToolBar.backgroundColor = UIColor.white
+        datePickerToolBar.tintColor = UIColor.blue
+        let doneButton = UIBarButtonItem.init(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(removeDatePicker));
+        datePickerToolBar.setItems([doneButton], animated: true)
+        self.view.addSubview(datePickerToolBar)
+    }
+    
+    func removeDatePicker() {
+        showingDatePicker = false
+        self.datePickerToolBar.removeFromSuperview()
+        self.datePicker.removeFromSuperview()
+    }
+    
+    func updateDateLabelTitle() {
+        let dateFormatter = DateFormatter.init()
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        let dateString = String.localizedStringWithFormat("%@", dateFormatter.string(from: datePicker.date))
+        self.dateLabel.text = dateString
+
+    }
 
     var searchController:UISearchController!
     var annotation:MKAnnotation!
