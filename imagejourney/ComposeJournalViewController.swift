f5f26386880d85bfdcb996fce32d4cb390096162
James Man <huaxiangman@pinterest.com>
2017-05-16 21:34:21 -0700

    bug fixing

--- imagejourney/ComposeJournalViewController.swift
+++ imagejourney/ComposeJournalViewController.swift
@@ -34,7 +34,7 @@ class ComposeJournalViewController: UIViewController {
     
     @IBAction func onCreateJournal(_ sender: UIBarButtonItem) {
         if titleTextField.text != nil && (titleTextField.text?.trimmingCharacters(in: .whitespaces).characters.count)! > 0 {
-            ParseClient.sharedInstance.saveJournal(title: titleTextField.text!, desc:descriptionTextField.text!, longtitude:longtitude!, latitude: latitude!, entries: entries!, previewImageUrls: previewImageUrls!, completion: { (pfJournalObj: PFObject) in
+            ParseClient.sharedInstance.saveJournal(title: titleTextField.text!, desc:descriptionTextField.text!, longtitude:longtitude, latitude: latitude, entries: entries!, previewImageUrls: previewImageUrls!, completion: { (pfJournalObj: PFObject) in
                 // On success of saving object to Parse, create Journal object from the PFObject we saved and pass it delegate method in HFVC which
                 // will  segue to after dismissing the Journal creation modal
                 let newJournal = Journal.init(obj: pfJournalObj)
